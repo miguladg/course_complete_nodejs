5aa57a945fc91d0159079926cc26ec085c91d557
miguel <migul.a.d.g@gmail.com>
2023-11-13 14:45:55 -0500

    importing your own files

---
+++ Utils.js
@@ -0,0 +1,11 @@
+console.log('utils.js')
+// This is the way to connect variables files fot a apped to big 
+
+const name = 'Mike'
+
+const add = function (a, b){
+    return a + b
+}
+
+module.exports = add
+// We this module ask me I send name becasu is I have
---
+++ app.js
@@ -0,0 +1,10 @@
+const add = require('./Utils')
+// It's necesari change the name' funtion because is more clear for us and can understantr better, thanks
+// Remenber we can change the name of the funtion for the funtion.
+// const name = 'Andrew'
+// this is first require happens up here and that's when Utils runs.
+// albeit a simple one that takes advantage of mutiple files.
+
+console.log(firstName)
+// If you dont put the good varobles show the name funtion
+// for sample if this funtion the varibles the name is fistNme
